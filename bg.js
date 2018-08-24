b8b9ca736ac6e7c3030d451b3bbc013ab4d16d72
Trishul <trishul.goel@gmail.com>
2018-08-24 15:54:10 +0200

    adding keyboard shortcuts

--- bg.js
+++ bg.js
@@ -1,3 +1,17 @@
 browser.browserAction.onClicked.addListener(tab => {
   browser.tabs.executeScript(tab.Id, {file: 'cs.js'});
 });
+
+chrome.commands.onCommand.addListener(command => {
+  if(command === 'open-measure-it') {
+    try{
+      browser.tabs.query({
+        active: true,
+        currentWindow: true
+      }).then(tab => {
+        const { id } = tab[0];
+        browser.tabs.executeScript(id, {file: 'cs.js'});
+      })
+    } catch(e){}
+  }
+});
