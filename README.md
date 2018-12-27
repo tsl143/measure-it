8e281146e094ea053cf7bbf0884908e132c8e44b
Trishul <trishul.goel@gmail.com>
2018-12-27 10:21:02 +0100

    GPLv3 licensing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Measure-It
-Source Code for MeasureIt addon at AMO
+Draw a ruler across any webpage to check the width, height, or alignment of page elements in pixels.
+
+[<img src="./repoAssets/amo.png">](https://addons.mozilla.org/en-US/firefox/addon/measure-it)
+[<img src="./repoAssets/chrome.png">](https://chrome.google.com/webstore/detail/measure-it/jocbgkoackihphodedlefohapackjmna)
+
+## License
+
+See [LICENSE](LICENSE) file.
--- cs.js
+++ cs.js
@@ -1,3 +1,23 @@
+/*******************************************************************************
+    Measure=it - A browser extension to measure parts of page.
+    Copyright (C) 2017-2019 Trishul Goel
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see {http://www.gnu.org/licenses/}.
+
+    Home: https://github.com/tsl143/measure-it
+*******************************************************************************/
+
 (function(){
   let x1 = 0;
   let x2 = 0;
